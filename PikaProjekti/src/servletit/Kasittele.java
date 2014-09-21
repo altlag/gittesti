55be810ac67e3d9de5ae2b51fe04d2962f6c5abd
altlag <altti.lagstedt@haagahelia.amk>
2014-09-21 19:45:37 +0300

    Kommentteja lisää

--- PikaProjekti/src/servletit/Kasittele.java
+++ PikaProjekti/src/servletit/Kasittele.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+//servlettiharjoitus
 /**
  * Servlet implementation class Kasittele
  * @author altlag
